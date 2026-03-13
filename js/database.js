44ba0a9de66510094fb3468b7132e9eb3d2026de
MICHEXE <nardellamichele92@gmail.com>
2026-03-13 13:17:03 +0100

    aggiornamento db

--- js/database.js
+++ js/database.js
@@ -7,7 +7,7 @@ const catalogoProdotti = [
         nome: "PRODOTTO1", 
         luogo: "ROMA, IT", 
         info: "DISPONIBILE", 
-        img: "../img/prodotto1.jpg", 
+        img: "https://cdn.jsdelivr.net/gh/MICHEXE/mini-app-assets/img/prodotto1.jpg", 
         desc: "In una piccola cittadina, c'era un giovane di nome Luca. Luca era sempre stato un sognatore, un ragazzo con la testa piena di idee e progetti. Tuttavia, c'era una cosa che lo bloccava: la paura del giudizio degli altri. Ogni volta che pensava di avviare un nuovo progetto, si lasciava sopraffare dai dubbi" 
 
     },
@@ -16,7 +16,7 @@ const catalogoProdotti = [
         nome: "PRODOTTO2", 
         luogo: "ROMA, IT", 
         info: "EDIZIONE LIMITATA", 
-        img: "../img/prodotto2.jpg", 
+        img: "https://cdn.jsdelivr.net/gh/MICHEXE/mini-app-assets/img/prodotto2.jpg", 
         desc: "Descrizione completa del prodotto..." 
     },
 
@@ -25,7 +25,7 @@ const catalogoProdotti = [
         nome: "PRODOTTO3", 
         luogo: "ROMA, IT", 
         info: "NUOVO", 
-        img: "../img/prodotto3.jpg", 
+        img: "https://cdn.jsdelivr.net/gh/MICHEXE/mini-app-assets/img/prodotto3.jpg", 
         desc: "Descrizione completa del prodotto..."
     }
 
