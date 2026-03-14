41eb01e22efba75c1f21c682c1d9a1213ee0ae2a
MICHEXE <nardellamichele92@gmail.com>
2026-03-14 14:07:07 +0100

    d

--- html/home.php
+++ html/home.php
@@ -11,7 +11,7 @@
         // =============================================================
         // L'UNICA COSA DA CAMBIARE PER AGGIORNARE TUTTI GLI ASSET:
         // =============================================================
-        const APP_VERSION = "4.4"; 
+        const APP_VERSION = "4.5"; 
 
         function caricaAsset(tipo, url) {
             return new Promise((resolve, reject) => {
