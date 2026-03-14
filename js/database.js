d748b7411cdd7e105838e3c20687249d2958018d
MICHEXE <nardellamichele92@gmail.com>
2026-03-14 13:08:14 +0100

    f

--- js/database.js
+++ js/database.js
@@ -10,7 +10,7 @@ const catalogoProdotti = [
         nome: "Prodotto 1", 
         luogo: "ROMA, IT", 
         info: "DISPONIBILE", 
-        img: "https://raw.githubusercontent.com/MICHEXE/mini-app-assets/main/img/prodotto1.jpg",
+        img: "https://raw.githubusercontent.com/MICHEXE/mini-app-assets/main/img/prodotto2.jpg",
         desc: "In una piccola cittadina, c'era un giovane di nome Luca. Luca era sempre stato un sognatore, un ragazzo con la testa piena di idee e progetti. Tuttavia, c'era una cosa che lo bloccava: la paura del giudizio degli altri. Ogni volta che pensava di avviare un nuovo progetto, si lasciava sopraffare dai dubbi" 
 
     },
